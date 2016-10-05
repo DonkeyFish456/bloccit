c1cd00c65c5fc71b66d4248cbd67dfec966bb57e
DonkeyFish456 <stancalderon@gmail.com>
2016-10-04 20:36:09 -0700

    add rspec, and start TDD for index, about, and faq views.

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+  describe 'Get Index' do
+    it 'should render the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'Get about' do
+    it 'should render the about template' do
+      get :about
+      expect(response).to render_template('about')
+    end
+  end
+
+  describe 'Get faq' do
+    it 'should render the faq template' do
+      get :faq
+      expect(response).to render_template('faq')
+    end
+  end
+end
