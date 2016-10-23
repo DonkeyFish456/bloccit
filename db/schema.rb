98fa45d79fc97b5b3b55997506937f4450611949
DonkeyFish456 <stancalderon@gmail.com>
2016-10-22 21:27:24 -0700

    add users to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016222400) do
+ActiveRecord::Schema.define(version: 20161023000524) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -45,9 +45,11 @@ ActiveRecord::Schema.define(version: 20161016222400) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 require 'random_data'
 
+#Create Users
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 #Create Topics
 15.times do
   Topic.create!(
@@ -22,6 +32,7 @@ end
 #Create Posts
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -48,7 +59,13 @@ end
 
 Post.find_or_create_by(title: 'unique title', body: 'unique body' )
 
+user = User.first
+user.update_attributes!(
+    email: 'stancalderon@gmail.com',
+    password: 'password'
+)
 puts 'Seed finished'
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Post.count} posts created"
